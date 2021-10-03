6be7a7bcb6cf6cab8995e9f5d1f9b16499ba8e99
Nemanja Manojlovic <manojlovic911@hotmail.com>
2021-10-03 19:11:46 +0200

    Add text prop to the Todo component

--- src/App.js
+++ src/App.js
@@ -2,8 +2,14 @@ import Todo from "./components/Todo";
 
 function App() {
     return (
-        <Todo/>
-        );
+        <div>
+            <h1>My Todos</h1>
+            <Todo text={"Example text string title"}/>
+            <Todo text={"Example text string title 2"}/>
+            <Todo text={"Example text string title 3"}/>
+            <Todo text={"Example text string title 4"}/>
+        </div>
+    );
 }
 
 export default App;
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,9 +1,8 @@
-function Todo() {
+function Todo(props) {
     return (
         <div>
-            <h1>My Todos</h1>
             <div className={'card'}>
-                <h2>Title</h2>
+                <h2>{props.text}</h2>
                 <div className={'actions'}>
                     <button className={'btn'}>Delete</button>
 
