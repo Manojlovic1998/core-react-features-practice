b2c9ec1fbcdf0329ba2ac0d6329f88d9c12e3f63
Nemanja Manojlovic <manojlovic911@hotmail.com>
2021-10-04 01:22:43 +0200

    Setup routing using the pages and react-router-dom package

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import {Route, Switch} from 'react-router-dom';
+import AllMeetupsPage from "./pages/AllMeetups";
+import FavoritesPage from "./pages/Favorites";
+import NewMeetupsPage from "./pages/NewMeetups";
 
 function App() {
     return (
         <div>
+            <Switch>
+                <Route path={'/'} exact={true}>
+                    <AllMeetupsPage />
+                </Route>
+                <Route path={'/favorites'}>
+                    <FavoritesPage />
+                </Route>
+                <Route path={'/new-meetups'}>
+                    <NewMeetupsPage />
+                </Route>
+            </Switch>
         </div>
     );
 }
