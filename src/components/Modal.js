8eb943aa75b072a6d1c7bcfd7d55add58f95da6e
Nemanja Manojlovic <manojlovic911@hotmail.com>
2021-10-04 00:59:57 +0200

    Delete componets and files before moving to routing

--- src/components/Modal.js
+++
@@ -1,9 +0,0 @@
-function Modal(props) {
-    return (<div className={'modal'}>
-        <p>Are you sure?</p>
-        <button className={"btn btn--alt"} onClick={props.onClick}>Cancel</button>
-        <button className={"btn"}>Confirm</button>
-    </div>)
-}
-
-export default Modal;
