150a8cd157f5876c52005d5e2bf647865ce9ab9d
Nemanja Manojlovic <manojlovic911@hotmail.com>
2021-10-04 01:00:24 +0200

    Delete imports of deleted components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
-import Todo from "./components/Todo";
 
 function App() {
     return (
         <div>
-            <h1>My Todos</h1>
-            <Todo text={"Example text string title"}/>
-            <Todo text={"Example text string title 2"}/>
-            <Todo text={"Example text string title 3"}/>
-            <Todo text={"Example text string title 4"}/>
         </div>
     );
 }
