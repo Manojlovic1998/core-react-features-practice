6bc9459e8fb0e9b19f6c538be6c1e4e8c27fe90c
Nemanja Manojlovic <manojlovic911@hotmail.com>
2021-10-07 01:49:19 +0200

    Use favorites context data to handle toggle button logic

--- src/components/meetups/MeetupItem.js
+++ src/components/meetups/MeetupItem.js
@@ -1,7 +1,26 @@
+import {useContext} from "react";
+import FavoritesContext from "../../store/favorites-context";
 import classes from "./MeetupItem.module.css";
 import Card from "../ui/Card";
 
 function MeetupItem(props) {
+    const favoritesCtx = useContext(FavoritesContext);
+    const itemIsFavorite = favoritesCtx.itemIsFavorite(props.id);
+
+    function toggleFavoriteStatusHandler() {
+        if (itemIsFavorite) {
+            favoritesCtx.removeFavorite(props.id);
+        } else {
+            favoritesCtx.addFavorite({
+                id: props.id,
+                title: props.title,
+                description: props.description,
+                image: props.image,
+                address: props.address,
+            });
+        }
+    }
+
     return (
         <li className={classes.item}>
             <Card>
@@ -14,8 +33,8 @@ function MeetupItem(props) {
                     <p>{props.description}</p>
                 </div>
                 <div className={classes.actions}>
-                    <button>
-                        To Favorites
+                    <button onClick={toggleFavoriteStatusHandler}>
+                        {itemIsFavorite ? "Remove From Favorites" : "Add To Favorites"}
                     </button>
                 </div>
             </Card>
