6848cc4551c1a039cf3ad40f97f484046046a4e4
Nemanja Manojlovic <manojlovic911@hotmail.com>
2021-10-06 22:49:52 +0200

    Fetch data from firebase and pass it through props

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,31 +1,45 @@
+import {useEffect, useState} from "react";
 import MeetupList from "../components/meetups/MeetupList";
+import stashed_urls from "../stashed_urls";
 
 function AllMeetupsPage() {
-    const DUMMY_DATA = [
-        {
-            id: 'm1',
-            title: 'This is a first meetup',
-            image:
-                'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
-            address: 'Meetupstreet 5, 12345 Meetup City',
-            description:
-                'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
-        },
-        {
-            id: 'm2',
-            title: 'This is a second meetup',
-            image:
-                'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
-            address: 'Meetupstreet 5, 12345 Meetup City',
-            description:
-                'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
-        },
-    ];
+    const [isLoading, setIsLoading] = useState(true);
+    const [fetchedData, setFetchedData] = useState();
 
+    useEffect(() => {
+        // Fetch the data from Firebase
+        fetch(stashed_urls + '/meetups.json').then((response) => {
+            return response.json();
+        }).then((res) => {
+            const meetups = [];
+
+            for (const key in res) {
+                const meetup = {
+                    id: key,
+                    ...res[key]
+                };
+
+                meetups.push(meetup);
+            }
+
+            setFetchedData(meetups);
+            setIsLoading(false);
+        });
+    }, []);
+
+
+    // Loading text
+    if (isLoading) {
+        return (<section>
+            <p>Loading...</p>
+        </section>);
+    }
+
+    // Once Data is loaded
     return (
         <section>
             <h1>All Meetups</h1>
-            <MeetupList meetups={DUMMY_DATA}/>
+            <MeetupList meetups={fetchedData}/>
         </section>
     );
 }
