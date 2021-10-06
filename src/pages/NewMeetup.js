24bd6ee049f32cb7a35d02050cd445107cb15ccc
Nemanja Manojlovic <manojlovic911@hotmail.com>
2021-10-06 19:51:56 +0200

    Add useHistory hook to redirect upon form submit

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,8 +1,12 @@
+import {useHistory} from "react-router-dom";
+
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 import Firebase_url from "../stashed_urls";
 
 
 function NewMeetupPage() {
+    let history = useHistory();
+
     function addMeetupForm(meetupData) {
         fetch(
             Firebase_url + '/meetups.json',
@@ -13,7 +17,9 @@ function NewMeetupPage() {
                     "Content-type": "application/json",
                 }
             }
-        );
+        ).then(value => {
+            history.push("/");
+        });
     }
 
     return (
